376f153d2a6bf2e020e3ee1b2a5acf0d8dce25ff
connorg2404 <connortownson@gmail.com>
2024-02-27 17:22:41 -0500

    added my goals to get done for today's class
    Mostly finishing the movement, as well as adding functionality to default pause/start menus

--- StorageSquad/Assets/CharacterMovement.cs
+++ StorageSquad/Assets/CharacterMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -22,6 +24,16 @@ public class CharacterMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
             if ((double) character.transform.position.x < 5) {
                 character.transform.Translate(1,0,0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
+            if ((double) character.transform.position.y < 3) {
+                character.transform.Translate(0,1,0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
+            if ((double) character.transform.position.y > -3) {
+                character.transform.Translate(0,-1,0);
             }
         }
     }
